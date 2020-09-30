4bdab6e976528c8db8a0e61fbdd14565b732dd86
schalon <steph_chalon@hotmail.com>
2020-09-30 11:44:46 +0200

    Gestion des exceptions dans la coroutine

--- src/main/kotlin/fr/convergence/proddoc/kafka/SurchargeReactive.kt
+++ src/main/kotlin/fr/convergence/proddoc/kafka/SurchargeReactive.kt
@@ -2,21 +2,15 @@ package fr.convergence.proddoc.kafka
 
 import fr.convergence.proddoc.model.SurchargeDemande
 import fr.convergence.proddoc.model.SurchargeReponse
-import fr.convergence.proddoc.model.lib.obj.MaskEntete
 import fr.convergence.proddoc.model.lib.obj.MaskMessage
-import fr.convergence.proddoc.model.lib.obj.MaskReponse
 import fr.convergence.proddoc.service.SurchargeService
-import fr.convergence.proddoc.util.maskIOHandler
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToJsonElement
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.slf4j.LoggerFactory
 import java.io.File
-import java.time.LocalDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
@@ -34,39 +28,39 @@ class SurchargeReactive(
     }
 
     @Incoming("surcharge")
-    fun consume(message: MaskMessage): MaskMessage = maskIOHandler(message) {
+    fun consume(messageOrigine: MaskMessage) {
 
         LOG.info("Demande de surcharge")
 
+        var messageRetour: MaskMessage = messageOrigine
+
         GlobalScope.launch {
-            val demandeSurcharge = message.recupererObjetMetier<SurchargeDemande>()
-            LOG.info("La demande est $demandeSurcharge")
+            try {
+                val demandeSurcharge = messageOrigine.recupererObjetMetier<SurchargeDemande>()
 
-            val documentModifie = surchargeService.appliquerSurcharge(demandeSurcharge)
+                LOG.info("La demande est $demandeSurcharge")
 
-            val fichierTemp = createTempFile(suffix = ".pdf", directory = File("c:\\TEMP\\"))
-            fichierTemp.writeBytes(documentModifie)
+                val documentModifie = surchargeService.appliquerSurcharge(demandeSurcharge)
 
-            LOG.info(fichierTemp.path)
-            retour(message, SurchargeReponse(fichierTemp.path))
+                val fichierTemp = createTempFile(suffix = ".pdf", directory = File("c:\\TEMP\\"))
+                fichierTemp.writeBytes(documentModifie)
+
+                LOG.info(fichierTemp.path)
+                messageRetour = MaskMessage.reponseOk(SurchargeReponse(fichierTemp.path), messageOrigine)
+
+            } catch (ex: Exception) {
+                messageRetour = MaskMessage.reponseKo<Exception>(ex, messageOrigine)
+            } finally {
+                retour(messageRetour)
+            }
         }
 
     }
 
-    private suspend fun retour(message: MaskMessage, reponseAsync: SurchargeReponse) {
+    private suspend fun retour(message: MaskMessage) {
 
-        LOG.info("Reponse asynchrone = $reponseAsync")
-
-        val messageRetour = MaskMessage(
-            MaskEntete(
-                message.entete.idUnique, message.entete.idLot, LocalDateTime.now(),
-                message.entete.idEmetteur, message.entete.idGreffe, message.entete.typeDemande
-            ),
-            Json.encodeToJsonElement(reponseAsync),
-            MaskReponse((true))
-        )
-
-        retourEmitter?.send(messageRetour)
+        LOG.info("Reponse asynchrone = $message")
+        retourEmitter?.send(message)
     }
 
 }
